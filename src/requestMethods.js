3eca3112f304d630b3fbd038c5980733429128de
User <you@example.com>
2023-07-15 16:47:48 +0800

    user list

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,14 +1,8 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:5001/api/';
-let TOKEN = '';
-
-// Retrieve the access token from local storage
-const localStorageData = JSON.parse(localStorage.getItem('persist:root'));
-const user = localStorageData ? JSON.parse(localStorageData.user) : null;
-TOKEN = user && user.currentUser ? user.currentUser.accessToken : '';
-console.log(user);
-console.log(TOKEN);
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)
+  .currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
@@ -16,5 +10,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  headers: { Authorization: `Bearer ${TOKEN}` },
+  headers: { token: `Bearer ${TOKEN}` },
 });
