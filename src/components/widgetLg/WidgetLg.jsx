5f3b8935f2537992852842fbf07631c8c69bc28b
User <you@example.com>
2023-07-16 10:18:54 +0800

    use lg

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -1,79 +1,54 @@
-import "./widgetLg.css";
+import React, { useEffect, useState } from 'react';
+import { userRequest } from '../../requestMethods';
+import './widgetLg.css';
+import Moment from 'react-moment';
 
 export default function WidgetLg() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const getOrders = async () => {
+      try {
+        const res = await userRequest.get('orders');
+        setOrders(res.data);
+      } catch (error) {
+        console.error('Error fetching orders:', error);
+      }
+    };
+    getOrders();
+  }, []);
+
   const Button = ({ type }) => {
-    return <button className={"widgetLgButton " + type}>{type}</button>;
+    return <button className={'widgetLgButton ' + type}>{type}</button>;
   };
+
   return (
     <div className="widgetLg">
       <h3 className="widgetLgTitle">Latest transactions</h3>
       <table className="widgetLgTable">
-        <tr className="widgetLgTr">
-          <th className="widgetLgTh">Customer</th>
-          <th className="widgetLgTh">Date</th>
-          <th className="widgetLgTh">Amount</th>
-          <th className="widgetLgTh">Status</th>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Susan Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Approved" />
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Susan Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Declined" />
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Susan Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Pending" />
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Susan Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Approved" />
-          </td>
-        </tr>
+        <tbody>
+          {/* Add tbody wrapper here */}
+          <tr className="widgetLgTr">
+            <th className="widgetLgTh">Customer</th>
+            <th className="widgetLgTh">Date</th>
+            <th className="widgetLgTh">Amount</th>
+            <th className="widgetLgTh">Status</th>
+          </tr>
+          {orders.map((order) => (
+            <tr className="widgetLgTr" key={order._id}>
+              <td className="widgetLgUser">
+                <span className="widgetLgName">{order.userId}</span>
+              </td>
+              <td className="widgetLgDate">
+                <Moment format="YYYY-MM-DD HH:mm A">{order.createdAt}</Moment>
+              </td>
+              <td className="widgetLgAmount">${order.amount}</td>
+              <td className="widgetLgStatus">
+                <Button type={order.status} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
